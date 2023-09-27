14acbae886c279b6f676f8cb9974fdd5f39a7e3e
Azam Sufiev <asufiev@rightandabove.com>
2023-09-28 01:11:43 +0200

    fixed the view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,20 +50,24 @@ export default function Home() {
               </button>
             ))}
           </div>
-          <div>
-            <input
-              type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="Enter HDH"
-              onChange={(event) => setvalue(Number(event.target.value))}
-            />
-          </div>
-          <button
-            onClick={() => (value && write ? write() : "")}
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Add DHM
-          </button>
+          {isConnected && (
+            <div>
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Enter HDH"
+                onChange={(event) => setvalue(Number(event.target.value))}
+              />
+            </div>
+          )}
+          {isConnected && (
+            <button
+              onClick={() => (value && write ? write() : "")}
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add DHM
+            </button>
+          )}
         </div>
       </div>
     </div>
