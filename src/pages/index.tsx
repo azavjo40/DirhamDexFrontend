42299e2de715b909d7d4a588a1fce44800b434fa
Azam Sufiev <asufiev@rightandabove.com>
2023-10-07 14:32:08 +0200

    fixed the rate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -146,8 +146,7 @@ export default function Home() {
               onClick={() => buy && buy()}
               className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              {/* {isLoadingBuy ? "Paying..." : "Pay  " + Number(value) * (rate?.toNumber() ?? 0) + " USDT"} */}
-              {isLoadingBuy ? "Paying..." : "Pay  " + Number(value) + " USDT"}
+              {isLoadingBuy ? "Paying..." : "Pay  " + Number(value) * (rate?.toNumber() ?? 0) + " USDT"}
             </button>
           )}
         </div>
